7e1e6ffd63c3a53c3e70ee5ef698d30abce79308
derbroti <derbroti@github.bitkrieger.de>
2022-03-03 11:54:59 +0100

    Only unzoom window if zoomed; make session name configurable; position popup right below status line

--- .tmux/pop.sh
+++ .tmux/pop.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
+POPSESS="-_popup_-"
 POPS=$(tmux showenv -g POPS)
 SESS=$(tmux display-message -p "#{session_id}")
 
 if [ -z "$POPS" ]; then
     tmux resize-pane -Z
-    tmux has-session -t popup
+    tmux has-session -t ${POPSESS}
     if [ $? -ne 0 ]; then
-        tmux new -ds popup
+        tmux new -ds ${POPSESS}
     fi
-    tmux switch-client -t popup
+    tmux switch-client -t ${POPSESS}
     TTY=$(tmux display-message -p "#{pane_tty}")
     sleep 0.15
     echo -ne "\033[2J" > $TTY
-    tmux display-popup -xC -y81 -b'rounded' -h'99%' -E "tmux attach -t '${SESS}' \; select-pane \; setenv -Fg POPS \"#{client_pid}\" \; set status off \; set pane-border-status off"
+    tmux display-popup -xC -yS -b'rounded' -h'99%' -w'40%' -E "tmux attach -t '${SESS}' \; select-pane \; setenv -Fg POPS \"#{client_pid}\" \; set status off \; set pane-border-status off"
 else
     PID=$(tmux showenv -g POPS | cut -d'=' -f2)
     ps -p $PID > /dev/null
     if [ $? -eq 0 ]; then
-        tmux resize-pane -Z
+        tmux if -F '#{window_zoomed_flag}' 'resize-pane -Z'
         tmux detach
         tmux switch-client -t ${SESS}
         tmux set status on
         tmux set pane-border-status bottom
-        tmux kill-session -t popup
+        tmux kill-session -t ${POPSESS}
     fi
     tmux setenv -gu POPS
 fi
